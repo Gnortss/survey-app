20f5374b411bf564dfaee636b6648dde961aa7a9
Gnortss <jure.casar1@gmail.com>
2023-11-09 16:44:41 +0100

    cloudflare pages test

---
+++ src/app.d.ts
@@ -0,0 +1,15 @@
+// See https://kit.svelte.dev/docs/types#app
+// for information about these interfaces
+declare global {
+	namespace App {
+		// interface Error {}
+		// interface Locals {}
+		// interface PageData {}
+		interface Platform {
+			env: {};
+			caches: CacheStorage & { default: Cache }
+		}
+	}
+}
+
+export {};
